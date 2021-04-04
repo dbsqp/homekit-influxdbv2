6e8245ba177fa3b3f204db1851093b9c6ccbc343
dbsqp <m.parkinson@mac.com>
2021-04-04 15:17:55 +0200

    Update homekit2influxdb.py
    update to eval env variable via string

--- homekit2influxdb.py
+++ homekit2influxdb.py
@@ -34,8 +34,10 @@ else:
 
 
 # HomeKit envionment variables
-homekit_ip_list=eval(os.getenv('HOMEKIT_IP_LIST', ""))
-homekit_host_list=eval(os.getenv('HOMEKIT_HOST_LIST', ""))
+homekit_ip_list_str=os.getenv('HOMEKIT_IP_LIST', "")
+homekit_host_list_str=os.getenv('HOMEKIT_HOST_LIST', "")
+homekit_ip_list=eval(homekit_ip_list_str)
+homekit_host_list=eval(homekit_host_list_str)
 
 
 # influxDBv2 envionment variables
